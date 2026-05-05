8b3e2fa608f15e0b13daf52cccb037e40dafceb5
Hassane <hassaneelkhatib@hotmail.com>
2026-05-05 04:38:19 +0300

    Fix terraform formatting

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -8,8 +8,8 @@ terraform {
     }
   }
 
-  
-backend "s3" {
+
+  backend "s3" {
     bucket         = "cicd-lab-tfstate-helkhatib-abc123"
     key            = "prod/terraform.tfstate"
     region         = "eu-north-1"
